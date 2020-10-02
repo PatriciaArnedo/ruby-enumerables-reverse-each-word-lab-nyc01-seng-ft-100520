0ba9d6142f4257f4837690f4992f0692bea713d3
Patricia Arnedo <paaarnedo@gmail.com>
2020-10-02 08:07:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,8 @@ def reverse_each_word(string)
   
   array = string.split
   
-  array.collect {|word| word.reverse }
+  new_array = array.collect {|word| word.reverse}
+  
+  return new_array.join(" ")
   
 end
